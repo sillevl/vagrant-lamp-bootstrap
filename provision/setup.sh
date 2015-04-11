cce6db930eec4c0e94d2fdae21b1021fbdece6e6
Sille Van Landschoot <info@sillevl.be>
2015-04-11 19:52:54 +0200

    added comments to setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -3,67 +3,64 @@
 echo "Provisioning virtual machine..."
 
 # Use single quotes instead of double quotes to make it work with special-character passwords
-PASSWORD=''
+PASSWORD='dev'
 
-# create project folder
-echo "Creating project folder /var/www/"
-sudo mkdir "/var/www/"
 
 # update / upgrade
 echo "update and upgrade system"
-sudo apt-get update
-sudo apt-get -y upgrade
+sudo apt-get update &> /dev/null
+sudo apt-get -y upgrade &> /dev/null
 
 # install apache 2.5 and php 5.5
 echo "installing apache2"
-sudo apt-get install -y apache2
+sudo apt-get install -y apache2 &> /dev/null
 echo "installing php5"
-sudo apt-get install -y php5
+sudo apt-get install -y php5 &> /dev/null
 
 # install mysql and give password to installer
 echo "configuring mysql"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password password $PASSWORD"
 sudo debconf-set-selections <<< "mysql-server mysql-server/root_password_again password $PASSWORD"
 echo "installing mysql"
-sudo apt-get -y install mysql-server
-sudo apt-get install php5-mysql
+sudo apt-get -y install mysql-server &> /dev/null
+sudo apt-get -y install php5-mysql &> /dev/null
 
 # install phpmyadmin and give password(s) to installer
 # for simplicity I'm using the same password for mysql and phpmyadmin
-echo "configure phpmyadmin"
+echo "configuring phpmyadmin"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/dbconfig-install boolean true"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/app-password-confirm password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/admin-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/mysql/app-pass password $PASSWORD"
 sudo debconf-set-selections <<< "phpmyadmin phpmyadmin/reconfigure-webserver multiselect apache2"
 echo "installing phpmyadmin"
-sudo apt-get -y install phpmyadmin
+sudo apt-get -y install phpmyadmin &> /dev/null
 
 # setup hosts file
 echo "Creating apache configuration"
 VHOST=$(cat <<EOF
 <VirtualHost *:80>
-    DocumentRoot "/var/www/"
-    <Directory "/var/www/">
+    DocumentRoot "/var/www/html/"
+    <Directory "/var/www/html/">
         AllowOverride All
         Require all granted
     </Directory>
 </VirtualHost>
 EOF
 )
-echo "${VHOST}" > /etc/apache2/sites-available/000-default.conf
+echo "${VHOST}" &> /etc/apache2/sites-available/000-default.conf
 
 # enable mod_rewrite
 echo "enabling mod_rewrite"
-sudo a2enmod rewrite
+sudo a2enmod rewrite &> /dev/null
 
 # restart apache
 echo "restarting apache"
-service apache2 restart
+service apache2 restart &> /dev/null
 
 # install git
 echo "installing git"
-sudo apt-get -y install git
+sudo apt-get -y install git &> /dev/null
 
 
 echo "Finished provisioning."
